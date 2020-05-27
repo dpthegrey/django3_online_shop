bba65f68c0f59b36e46703cc1994dce4a9f2a68c
dpthegrey <dpthegrey@gmail.com>
2020-05-27 23:19:57 +0530

    Building catalog views

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+# A view to list all the products or filter products by a given product category.
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    # filter the QuerySet with available=True to retrieve only available products.
+    products = Product.objects.filter(available=True)
+    if category_slug:
+      # use an optional category_slug parameter to optionally filter products by a given category.
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {
+                      'category': category,
+                      'categories': categories,
+                      'products': products
+                  })
+
+
+# A view to retrieve and display a single product by ID.
+def product_detail(request, id_slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})

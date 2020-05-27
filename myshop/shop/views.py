e5b1e8e253f5d23d4a470867a932ab5089180466
dpthegrey <dpthegrey@gmail.com>
2020-05-28 00:36:01 +0530

    Creating catalog templates

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -22,7 +22,7 @@ def product_list(request, category_slug=None):
 
 
 # A view to retrieve and display a single product by ID.
-def product_detail(request, id_slug):
+def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
                                 slug=slug,
